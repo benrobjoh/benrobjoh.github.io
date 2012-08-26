c418b1020d5e64297f4aa911c7292fb35053e3e0
Ben Johnson <ben@benrobjoh.com>
2012-08-26 13:46:53 -0400

    Began using Sass for style sheets

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "images"
 javascripts_dir = "scripts"
 
 output_style = :nested
-environment = :development
+environment = :production
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
