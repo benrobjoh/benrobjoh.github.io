f754bbe97a347f1bdbd6bd5390ba32202edc8283
Ben Johnson <ben@benrobjoh.com>
2012-08-26 20:53:45 -0400

    Decreased top margin on benrobjoh banner

--- include/header.php
+++ include/header.php
@@ -1,17 +1,17 @@
 <nav>
-<ul id="navbar">
-	<li><a href="/">Home</a></li>
-	<li><a href="/contact">Contact</a></li>
-	<li><a href="/education">Education</a>
-		<ul>
-		<li><a href="/education/hhs">High School</a></li>
-		<li><a href="/education/etown">Undergraduate</a></li>
-		<li><a href="/education/unc">Graduate</a></li>
-		</ul>
-	</li>
-	<li><a href="/work">Work Experience</a></li>
-	<li><a href="/skills">Skills</a></li>
-	<li><a href="/resume/Johnson%20Resume.pdf" target="_blank" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'Resume']);">R&eacute;sum&eacute;</a></li>
+	<ul id="navbar">
+		<li><a href="/">Home</a></li>
+		<li><a href="/contact">Contact</a></li>
+		<li><a href="/education">Education</a>
+			<ul>
+				<li><a href="/education/hhs">High School</a></li>
+				<li><a href="/education/etown">Undergraduate</a></li>
+				<li><a href="/education/unc">Graduate</a></li>
+			</ul>
+		</li>
+		<li><a href="/work">Work Experience</a></li>
+		<li><a href="/skills">Skills</a></li>
+		<li><a href="/resume/Johnson%20Resume.pdf" target="_blank" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'Resume']);">R&eacute;sum&eacute;</a></li>
 	</ul>
 </nav>
 
