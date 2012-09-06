8ddebdabd659cbfb332751b198e71f8cb43cac7e
Ben Johnson <ben@benrobjoh.com>
2012-09-06 17:59:34 -0400

    Combined existing HTML into the single page file

--- singlepage.php
+++ singlepage.php
@@ -21,8 +21,295 @@
 				<li><a href="#work">Work Experience</a></li>
 				<li><a href="#skills">Skills</a></li>
 				<li><a href="#resume">R&eacute;sum&eacute;</a></li>
+				<li><a href="#contact">Contact</a></li>
 			</ul>
 		</nav>
 	</header>
+	<div id="content">
+		<section id="about">
+			<img src="Ben%20Johnson.jpg" alt="Ben Johnson Photo" height="278" width="500" />
+			<p>Ben Johnson, born November 9, 1988, grew up in Lancaster, PA. He graduated from Hempfield High School in June 2007. At Hempfield he earned four varsity letters in tennis, played oboe in the concert band, and was co-editor-in-chief of the newspaper. He completed his undergraduate studies at Elizabethtown College in three years, graduating with a BS in mathematics with a second major in economics and a minor in Spanish. After passing actuarial exam FM (financial mathematics), Ben interned with the actuarial department of Arch Insurance in New York, NY during the summer of 2009.</p>
+	<p>Upon graduation from Elizabethtown College, Ben began graduate school at the University of North Carolina at Chapel Hill. He graduated with an MS in economics in December 2011. At UNC, he studied applied microeconomics and wrote a Master's paper titled <a href="http://benrobjoh.com/education/unc/Student%20Debt.pdf" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'Student Debt: Who Borrows?']);">"Student Debt: Who Borrows?"</a>. He took courses in health economics, labor economics, empirical industrial organization, and econometrics.</p>
+		</section>
+		
+		<section id="education">
+			<div class="nav">
+				<ul>
+					<li><a href="#unc">UNC-Chapel Hill</a></li>
+					<li><a href="#etown">Elizabethtown College</a></li>
+					<li><a href="#hempfield">Hempfield</a></li>
+				</ul>
+			</div>
+			
+			<div id="unc">
+				<h1>University of North Carolina at Chapel Hill</h1>
+				<h2><b>MS Economics</b>, December 2011</h2>
+				<h2>Academics</h2>
+				
+				<ul>
+					<li>Master's Thesis: <em><a href="http://benrobjoh.com/education/unc/Student%20Debt.pdf" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'Student Debt: Who Borrows?']);">Student Debt: Who Borrows?</a></em>, <span id="debtabstractlink" class="abstractlink" onClick = "_gaq.push(['_trackEvent', 'View', 'Abstract', 'Student Debt: Who Borrows?']);">Abstract</span></li>
+					<p id="debtabstract" class="abstract">
+						This paper uses a selection model to investigate what characteristics of students affect the probability and amount of student debt. While most previous work in the area of student debt examines the effects of student debt on schooling and employment decisions, this paper looks at the demographic, financial, and psychological factors that determine whether a student uses debt to finance his education. The main findings are that increased savings does not decrease the probability or amount of student debt accrued and that increased family income decreases the probability of student debt, but increases the average amount of debt, given that the student has some debt.
+					</p>
+					<li>Health Economics Paper: <em><a href="http://benrobjoh.com/education/unc/Disability%20Insurance.pdf" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'Disability Insurance']);">Employment and Disability Insurance</a></em>, <span id="disabilityabstractlink" class="abstractlink" onClick = "_gaq.push(['_trackEvent', 'View', 'Abstract', 'Disability Insurance']);">Abstract</span></li>
+					<p id="disabilityabstract" class="abstract">
+						When an individual becomes disabled, he faces a decision whether to continue working or to apply for disability benefits through Social Security Disability Insurance (SSDI). The indi- vidual must consider tradeoffs between continuing to work and applying for SSDI benefits. If the individual continues to work, he might make his disability worse. If he applies for dis- ability benefits, he must quit his job, forgoing his wages and losing his accumulated tenure with his employer. He also faces the uncertainty of whether his SSDI application will be approved. This paper models the disabled individual's decision-making process.
+					</p>
+					<li>Microeconometrics Paper: <em><a href="http://benrobjoh.com/education/unc/Religion%20and%20Philanthropy.pdf" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'Religion and Philanthropy']);">Religion and Philanthropy</a></em>, <span id="philanthropyabstractlink" class="abstractlink" onClick = "_gaq.push(['_trackEvent', 'View', 'Abstract', 'Religion and Philanthropy']);">Abstract</span></li>
+					<p id="philanthropyabstract" class="abstract">
+						This paper uses a selection model and a Tobit model to investigate the effects of religion on the probability and amount of charitable donations. The main findings from the selection model are that being Catholic or Protestant make a family more likely to donate to charity than people who do not identify with a religion. Protestants, on average, donate more money to charity than people who do not identify with a religion. The Tobit model finds that Catholics, Jews, Protestants, and non-Christians give significantly more money to charity than non-religious people.
+					</p>
+					<li>Empirical Industrial Organization Paper: <em><a href="http://benrobjoh.com/education/unc/College%20Pricing.pdf" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'College Pricing']);">Price-Setting by Colleges</a></em>, <span id="collegeabstractlink" class="abstractlink" onClick = "_gaq.push(['_trackEvent', 'View', 'Abstract', 'College Pricing']);">Abstract</span></li>
+					<p id="collegeabstract" class="abstract">
+						Colleges in the United States price discriminate based on student characteristics such as ability and income. This paper develops a model of college pricing in which colleges maximize their prestige. Several policy experiments are considered in which the college loses the ability to price discriminate at all or to price discriminate with respect to income.
+					</p>
+				</ul>
+				
+				<h2>Teaching</h2>
+				<ul>
+					<li><b>Econ 410 Intermediate Microeconomic Theory</b>, spring 2012: ran weekly review sessions to answer student questions, graded homework and exams for a 500-student class</li>
+					<li><b>Econ 101 Introduction to Economics</b>, fall 2011: Taught three recitation sections, held office hours, created exam questions, graded exams, facilitated logistics associated with running a 400-student class</li>
+					<li><b>Econ 460 Economic Development</b>, spring 2011: Graded homework</li>
+					<li><b>Econ 423 Financial Markets</b>, fall 2010: Graded homework and exams, graded weekly reports associated with stock market investment project</li>
+				</ul>
+				<h2>Courses</h2>
+				<ul>
+					<li>Econ 700 Quantitative Techniques</li>
+					<li>Econ 710 Advanced Microeconomic Theory I: Choice theory</li>
+					<li>Econ 711 Advanced Microeconomic Theory II: Game theory</li>
+					<li>Econ 720 Advanced Macroeconomic Theory I: Growth theory</li>
+					<li>Econ 721 Advanced Macroeconomic Theory II: Monetary macroeconomics</li>
+					<li>Econ 770 Introduction to Econometrics: Probability</li>
+					<li>Econ 771 Econometrics: Applied econometrics, data analysis, Stata</li>
+					<li>Econ 850 Health Economics: Structural health models</li>
+					<li>Econ 870 Advanced Econometrics: Theoretical econometrics, MATLAB</li>
+					<li>Econ 871 Microeconometrics: Applied econometrics, data analysis</li>
+					<li>Econ 880 Labor Economics I: Reduced-form labor economics, data analysis, Stata</li>
+					<li>Econ 881 Labor Economics II: Structural labor economics, model estimation in MATLAB</li>
+					<li>Econ 890 Quantitative Methods</li>
+					<li>Econ 890 Empirical Industrial Organization: Survey of empirical industrial organization literature, MATLAB</li>
+				</ul>
+				<img src="UNC%20Graduation.jpg" alt="UNC Graduation Photo" height="460" width="750"/>
+			</div>
+			
+			<div id="etown">
+				<h1 class=>Elizabethtown College</h1>
+				<h2><b>BS Mathematics</b>, Summa Cum Laude, May 2010</h2>
+				<h3>Second Major in Economics and Minor in Spanish</h3>
+				<h2>Academics</h2>
+				<ul>
+					<li>Thesis for Honors in the Discipline of Economics: <em><a href="http://benrobjoh.com/education/etown/Economics%20of%20College%20Tuition%20and%20Finacial%20Aid.pdf" onClick = "_gaq.push(['_trackEvent', 'Downloads', 'PDF', 'Economics of College Tuition and Financial Aid']);">The Economics of College Tuition and Financial Aid</a></em>, <span id="tuitionabstractlink" class="abstractlink" onClick = "_gaq.push(['_trackEvent', 'View', 'Abstract', 'Economics of College Tuition and Financial Aid']);">Abstract</span></li>
+					<p id="tuitionabstract" class="abstract">This paper investigates the economics of college tuition and financial aid. In the United States, colleges set their tuition rates before each academic year. Most students, however, do not pay the full tuition rate, with 62.2% of full-time undergraduates receiving some type of financial aid in 2007-2008. Colleges price discriminate, attempting to figure out how much each particular student would be willing to pay, and charging that rate. College financial aid offices meet the difference between the full tuition rate and what the student is deemed able to pay with institutional loans, government-subsidized loans, and grants. This type of aid is based on financial need. The goal of providing financial aid to students is to ensure that lower-income students have an opportunity to attend college. In addition to this goal, colleges use price discrimination as a way to increase their revenue. This paper will explore the economic theory behind price discrimination and how the behavior of the college fits that of a profit-maximizing firm.  This paper will analyze what a student could expect to pay as a percentage of total income at different income levels and the effects of the financial aid system on the labor market and retirement saving.</p>
+				</ul>
+				<img src="Etown%20Graduation.jpg" alt="Etown Graduation Photo" class="floatRight"/>
+				<h2>Presentations</h2>
+				<ul>
+					<li>Elizabethtown College Scholarship Day
+						<ul>
+							<li><em>The Politics of Global Warming</em> (2008)</li>
+							<li><em>How Twentieth Century Spanish History Explains the Rivalry Between Real Madrid CF and FC Barcelona</em> (2009)</li>
+							<li><em>The Economics of College Tuition and Financial Aid</em> (2010)</li>
+						</ul>
+					</li>
+					<li>King's College Global Landscapes Conference
+						<ul>
+							<li><em>The Economics of College Tuition and Financial Aid</em> (2010)</li>
+						</ul>
+					</li>
+					<li>EPaDel Mathematical Association of America Conference
+						<ul>
+							<li><em>History of Baseball Statistics</em> (2010)</li>
+						</ul>
+					</li>
+				</ul>
+
+				<h2>Activities</h2>
+				<ul>
+					<li>Elizabethtown College Honors Program</li>
+					<li>Symphonic Band Oboist</li>
+					<li>College Fed Challenge (2009)</li>
+				</ul>
+				
+				<h2>Honors</h2>
+				<ul>
+					<li>Elizabethtown College Presidential Scholarship Recipient</li>
+					<li>Elizabethtown College Scholar</li>
+					<li>Distinguished Student of the Year nominee</li>
+					<li>Pi Mu Epsilon mathematics honor society member</li>
+					<li>Delta Mu Delta business honor society member</li>
+				</ul>
+				<h2>Courses</h2>
+				<ul>
+					<li>Mathematics
+						<ul>
+							<li>Calculus (I, II, III)</li> 
+							<li>Linear Algebra</li>
+							<li>Theory of Probability</li>
+							<li>Financial Mathematics</li>
+							<li>Statistical Methods in Research</li>
+							<li>Real Analysis</li>
+							<li>Abstract Algebra</li>
+							<li>History of Mathematics</li>
+							<li>Discrete Mathematics</li>
+						</ul>
+					</li>
+					<li>Economics
+						<ul>
+							<li>Microeconomics</li>
+							<li>Intermediate Microeconomics</li>
+							<li>Macroeconomics</li>
+							<li>Intermediate Macroeconomics</li>
+							<li>International Economics</li>
+							<li>Economic Development</li>
+							<li>Mathematical Economics</li>
+						</ul>
+					</li>
+					<li>Spanish
+						<ul>
+							<li>Literature</li>
+							<li>Conversation</li>
+							<li>Modern Society</li>
+						</ul>
+					</li>
+					<li>Other
+						<ul>
+							<li>Computer Science (C++)</li>
+							<li>Accounting</li>
+							<li>Honors Leadership</li>
+							<li>Honors Meteorology</li>
+							<li>Honors History of Modern Asia</li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			
+			<div id="hempfield">
+				<h1>Hempfield High School</h1>
+				<h2>Graduated June, 2007</h2>
+				<h2>Activities</h2>
+				<ul>
+					<li>Varsity tennis (four years)</li>
+					<li>Concert Band and Orchestra Oboist</li>
+					<li>Varsity Quiz Bowl</li>
+					<li>Editor-in-Chief of school newspaper</li>
+				</ul>
+				<h2>Honors</h2>
+				<ul>
+					<li>National Merit Finalist, 2006</li>
+					<li>Graduated in top ten of class of over 550</li>
+					<li>Winner of National Council of Teachers of English Achievement Award for Writing, 2006</li>
+				</ul>
+			</div>
+		
+		</section>
+		
+		<section id="work">
+			<h1>Work Experience</h1>
+			<h2>Economics Department, University of North Carolina at Chapel Hill</h2>
+			<ul>
+				<li>Teaching Assistant, 2010-2012
+					<ul>
+						<li><b>Econ 410 Intermediate Microeconomic Theory</b>, spring 2012: ran weekly review sessions to answer student questions, graded homework and exams</li>
+						<li><b>Econ 101 Introduction to Economics</b>, fall 2011: Taught three recitation sections, held office hours, created exam questions, graded exams, facilitated logistics associated with running a 400-student class</li>
+						<li><b>Econ 460 Economic Development</b>, spring 2011: Graded homework</li>
+						<li><b>Econ 423 Financial Markets</b>, fall 2010: Graded homework and exams, graded weekly reports associated with stock market investment project</li>
+					</ul>
+				</li>
+			</ul>
+			<h2>Arch Insurance Group, New York, NY</h2>
+			<ul>
+				<li>Actuarial Intern, summer 2009
+					<ul>
+						<li>Blended industry data with Arch loss experience to select Loss Development Factors, set up Arch LDF Library, presented findings to Corporate Actuarial group</li>
+					</ul>
+				</li>
+			</ul>
+			<h2>Hempfield Area Recreation Commission, Landisville, PA</h2>
+			<ul>
+				<li>National Junior Tennis League Instructor, summer 2004-2008, 2010-2012
+					<ul>
+						<li>Taught tennis to children ranging from age five to fifteen; managed an assistant instructor; constructed each day's lesson with age-appropriate tennis activities</li>
+					</ul>
+				</li>
+			</ul>
+			<h2>Department of Mathematical Sciences, Elizabethtown College</h2>
+			<ul>
+				<li>Math Lab Tutor, 2008-2009
+					<ul>
+						<li>Assisted students with mathematics homework</li>
+					</ul>
+				</li>
+			</ul>		
+		</section>
+		
+		<section id="skills">
+			<h1>Skills</h1>
+			<h2>Economics</h2>
+			<ul>
+				<li>Applied Econometrics</li>
+				<li>Health Economics</li>
+				<li>Labor Economics</li>
+				<li>Data Analysis</li>
+			</ul>
+			<h2>Software and Computer Skills</h2>
+			<ul>
+				<li>Mac OS X</li>
+				<li>iOS</li>
+				<li>Microsoft Office: Word, Excel, PowerPoint</li>
+				<li>Mathematics and Statistics Software
+					<ul>
+						<li><a href="http://en.wikipedia.org/wiki/Stata">Stata</a></li>
+						<li><a href="http://en.wikipedia.org/wiki/Matlab">MATLAB</a></li>
+					</ul>
+				</li>
+				<li><a href="http://www.latex-project.org/">LaTeX</a></li>
+				<li><a href="http://www.google.com/analytics/index.html">Google Analytics</a></li>
+				<li><a href="http://www.google.com/a">Google Apps</a></li>
+				<li>Website Administration</li>
+			</ul>
+			<h2>Programming</h2>
+			<ul>
+				<li>HTML and CSS</li>
+				<li>Javascript</li>
+				<li><a href="http://jquery.com/">jQuery</a></li>
+				<li>Objective-C for iOS development</li>
+				<li><a href="http://git-scm.com/">Git</a></li>
+			</ul>
+			<h2>Teaching</h2>
+			<ul>
+				<li>ACTFL intermediate high proficiency in Spanish</li>
+				<li>Teaching Tennis</li>
+				<li>Teaching Economics</li>
+			</ul>		
+		</section>
+		
+		<section id="resume">
+		
+		</section>
+		
+		<section id="contact">
+		
+		</section>
+	</div>
+	
+	<script>
+		$(document.ready(function() {
+			$('#debtabstractlink').click(function() {
+				$('#debtabstract').slideToggle(1000)
+			});
+			$('#disabilityabstractlink').click(function() {
+				$('#disabilityabstract').slideToggle(1000)
+			});
+			$('#philanthropyabstractlink').click(function() {
+				$('#philanthropyabstract').slideToggle(1000)
+			});
+			$('#collegeabstractlink').click(function() {
+				$('#collegeabstract').slideToggle(1000)
+			});
+			$('#tuitionabstractlink').click(function() {
+				$('#tuitionabstract').slideToggle(1000)
+			});
+		});
+	</script>
 </body>
 </html>
