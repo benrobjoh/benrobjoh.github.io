ef4689e2fc94739662040e4314dfd0ce1d071092
Ben Johnson <ben@benrobjoh.com>
2012-09-06 17:00:44 -0400

    Began converting to the single-page layout

---
+++ singlepage.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link href="http://benrobjoh.com/css/singlepage.css" media="screen, projection" rel="stylesheet" type="text/css" />
+	<link rel="shortcut icon" href="http://benrobjoh.com/include/favicon.ico"/>
+	<link rel="apple-touch-icon" href="http://benrobjoh.com/include/iosicon.png"/>
+	<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<title>benrobjoh</title>
+	<meta name="description" content="Ben Johnson" />
+	<meta name="keywords" content="" />
+	<meta charset="UTF-8" />
+</head>
+
+<body>
+	<header>
+		<h1>Ben Johnson</h1>
+		<nav>
+			<ul>
+				<li><a href="#about">About</a></li>
+				<li><a href="#education">Education</a></li>
+				<li><a href="#work">Work Experience</a></li>
+				<li><a href="#skills">Skills</a></li>
+				<li><a href="#resume">R&eacute;sum&eacute;</a></li>
+			</ul>
+		</nav>
+	</header>
+</body>
+</html>
